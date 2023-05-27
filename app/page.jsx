651357138e6006d57acc320acfbea0979eb0f782
DenisCorlade <corladedenis19@gmail.com>
2023-05-27 20:08:07 +0200

    Add Navigation component for desktop

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,5 @@
+import Feed from "@components/Feed"
+
 
 const Home = () => {
   return (
@@ -14,8 +16,8 @@ const Home = () => {
       <p className="desc text-center">
         Promptopia is an open-source AI prompting tool for modern world to discover, create and share creative prompts
       </p>
-
-      {/* Feed */}
+    
+    <Feed/>
     </section>
   )
 }
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,8 +1,86 @@
-import React from 'react'
+"use client";
+
+import Link from 'next/link';
+import Image from 'next/image';
+import {useState, useEffect} from 'react';
+import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 const Nav = () => {
+  const isUserLoggedIn = true;
+
+  const [providers, setProviders] = useState(null);
+
+  // only runs at the start due to []
+  useEffect(() => {
+    const setProviders = async () => {
+      const response = await getProviders();
+
+      setProviders(response);
+    }
+
+    setProviders();
+  }, []);
+
   return (
-    <div>Nav</div>
+    <nav className='flex-between w-full mb-16 pt-3'>
+      <Link href="/" className='flex gap-2 flex-center'>
+        <Image 
+          src="/assets/images/logo.svg" 
+          alt="Promptopia logo"
+          width={30}
+          height={30}
+          className="object-contain"
+        />
+        <p className='logo_text'>Promptopia</p>
+      </Link>
+
+      {/* Desktop navigation */}
+      {/* On small devices it is hidden, but on everything greate than small, it will show up */}
+      <div className='sm:flex hidden'>
+        {isUserLoggedIn ? (
+          <div className='flex gap-3 md:gap-5'>
+            <Link href="/create-prompt" className='black_btn'>
+              Create Post
+            </Link>
+
+            <button type="button" onClick={signOut} className="outline_btn">
+              Sign Out
+            </button>
+
+            <Link href="/profile">
+              <Image 
+                src="/assets/images/logo.svg" 
+                width={37}
+                height={37}
+                className="rounded-full"
+                alt="Profile picture"
+              />
+            </Link>
+
+          </div>
+        ) : (
+          <>
+          {/* Have all different providers and show buttons for signup here. 
+          Atm, only google signup will be here */}
+            {providers && 
+              Object.values(providers).map((provider) => (
+                <button
+                  type="button"
+                  key={provider.name}  
+                  onClick={() => signIn(provider.id)}
+                  className="black_btn"
+                >
+                  Sign In
+                </button>
+              ))
+            }
+          </>
+        )}
+      </div>
+
+      {/* Mobile Navigation */}
+
+    </nav>
   )
 }
 
