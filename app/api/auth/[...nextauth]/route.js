c36df0439f7db2a23cb1a40c28b03456c6030972
DenisCorlade <corladedenis19@gmail.com>
2023-05-30 17:58:08 +0200

    Create user model and add signup functionality

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,7 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 
+import User from '@models/User';
 import { ConnectToDatabase } from '@utils/database';
 
 const handler = NextAuth({
@@ -18,7 +19,20 @@ const handler = NextAuth({
             //route is serverless -> lambda that everytime it gets called, make connection to database
             await connectToDatabase();
 
-            // check if user already exists, if not, create new user
+            // check if user already exists, 
+            const userExists = await User.findOne({ 
+                email: profile.email 
+            });
+
+            // if not, create new user
+            if (!userExists) {
+                await User.create({
+                    email: profile.email,
+                    name: profile.name.replace(" ", "").toLowerCase(),
+                    image: profile.picture
+                });
+            }
+
 
             return true;
         } catch (err) {
