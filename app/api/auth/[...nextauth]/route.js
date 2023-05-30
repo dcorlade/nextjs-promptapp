e28fc20894030a65266c841718bbb291ead9b9fe
DenisCorlade <corladedenis19@gmail.com>
2023-05-30 19:48:46 +0200

    Repair connection to database
    link signin from google to mongodb

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -11,42 +11,40 @@ const handler = NextAuth({
             clientSecret: process.env.GOOGLE_CLIENT_SECRET, 
         })
     ],
-    async session({ session }) {
-        const sessionUser = await User.findOne({ 
-            email: session.user.email 
-        });
+    callbacks: {
+        async session({ session }) {
+            // store the user id from MongoDB to session
 
-        session.user.id = sessionUser._id.toString();
+            const sessionUser = await User.findOne({ email: session.user.email });
+            session.user.id = sessionUser._id.toString();
 
-        // update session so that we know the user is logged in
-        return session;
-    },
-    async signIn({ profile }) {
-        try {
-            //route is serverless -> lambda that everytime it gets called, make connection to database
-            await connectToDatabase();
+            return session;
+        },
+        async signIn({ profile }) {
+            try {
+                //route is serverless -> lambda that everytime it gets called, make connection to database
+                await ConnectToDatabase();
+                
+                // check if user already exists, 
+                const userExists = await User.findOne({ email: profile.email });
 
-            // check if user already exists, 
-            const userExists = await User.findOne({ 
-                email: profile.email 
-            });
+                // if not, create new user
+                if (!userExists) {
+                    await User.create({
+                      email: profile.email,
+                      username: profile.name.replace(" ", "").toLowerCase(),
+                      image: profile.picture,
+                    });
+                }
+    
+                return true;
 
-            // if not, create new user
-            if (!userExists) {
-                await User.create({
-                    email: profile.email,
-                    name: profile.name.replace(" ", "").toLowerCase(),
-                    image: profile.picture
-                });
+            } catch (error) {
+                console.log("Error checking if user exists: ", error.message);
+                return false
             }
-
-
-            return true;
-        } catch (err) {
-            console.log(err);
-            return false;
         }
-    }
+    },
 })
 
 export { handler as GET, handler as POST }
