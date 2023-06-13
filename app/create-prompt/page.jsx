4df0483efa5539be143fdbd00a0e13439b23c710
DenisCorlade <corladedenis19@gmail.com>
2023-06-13 18:51:28 +0300

    Add prompts to DB

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -5,8 +5,13 @@ import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
 
 import Form from '@components/Form'
+import Router from 'next/router';
 
 const CreatePrompt = () => {
+  const router = useRouter();
+  const {data: session} = useSession();
+
+
   const [submitting, setSubmitting] = useState(false);
   const [post, setPost] = useState({
     prompt: '',
@@ -14,7 +19,28 @@ const CreatePrompt = () => {
   })
 
   const createPrompt = async (e) => {
+    e.preventDefault(); // dont reload when form is submitted
+    setSubmitting(true);
 
+    try {
+      // post prompt to our api
+      const response = await fetch('/api/prompt/new', {
+        method: 'POST',
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userId: session?.user.id,
+          tag: post.tag
+        })
+      })
+
+      if (response.ok) {
+        router.push('/') // push to homepage
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setSubmitting(false); //after submitting the prompt, setSubmitting to false
+    }
   }
 
   return (
